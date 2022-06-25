b7916d2ff71fe6c823d11f7136d1810b459f865e
xIceArcher <xicearcher@gmail.com>
2022-06-26 01:57:13 +0800

    Add style modifier

--- modifier.py
+++ modifier.py
@@ -41,9 +41,14 @@ def parse_modifiers(s: str) -> list[Modifier]:
 
 class LineModifier:
     def __init__(self) -> None:
-        self.should_force_secondary = False
-        self.should_discard = False
+        self.shouldForceSecondary = False
+        self.shouldDiscard = False
+
         self.offset = timedelta(0)
+
+        self.shouldOverwriteStyle = False
+        self.breakpoints = None
+        self.actors = None
 
 def to_line_modifiers(modifiers: list[Modifier], maxLines=100) -> list[LineModifier]:
     ret = [LineModifier() for _ in range(maxLines)]
@@ -51,7 +56,7 @@ def to_line_modifiers(modifiers: list[Modifier], maxLines=100) -> list[LineModif
     for modifier in modifiers:
         if modifier.operation == 'discard':
             for i in range(modifier.start, modifier.end if modifier.end is not None else maxLines):
-                ret[i].should_discard = True
+                ret[i].shouldDiscard = True
         elif modifier.operation == 'offset':
             offsetStr = modifier.rest[0]
             offset = timedelta(seconds=pytimeparse.parse(offsetStr))
@@ -60,7 +65,28 @@ def to_line_modifiers(modifiers: list[Modifier], maxLines=100) -> list[LineModif
                 ret[i].offset += offset
         elif modifier.operation == 'secondary':
             for i in range(modifier.start, modifier.end if modifier.end is not None else maxLines):
-                ret[i].should_force_secondary = True
+                ret[i].shouldForceSecondary = True
+        elif modifier.operation == 'style':
+            if len(modifier.rest) == 1 and not ':' in modifier.rest[0]:
+                # Simple case, entire line is the same actor
+                actor = int(modifier.rest[0])
+                for i in range(modifier.start, modifier.end if modifier.end is not None else maxLines):
+                    ret[i].shouldOverwriteStyle = True
+                    ret[i].breakpoints = [0]
+                    ret[i].actors = [actor]
+            else:
+                breakpoints = []
+                actors = []
+
+                for breakpointToActorStr in modifier.rest:
+                    breakpoint, actor = breakpointToActorStr.split(':')
+                    breakpoints.append(int(breakpoint) - 1)
+                    actors.append(int(actor))
+
+                for i in range(modifier.start, modifier.end if modifier.end is not None else maxLines):
+                    ret[i].shouldOverwriteStyle = True
+                    ret[i].breakpoints = breakpoints
+                    ret[i].actors = actors
 
     return ret
 
@@ -70,14 +96,18 @@ def modify_song(songJson, modifiers: list[Modifier]):
     outDetailedLyrics = []
 
     for line, modifier in zip(songJson['lyrics']['detailed'], lineModifiers):
-        if modifier.should_discard:
+        if modifier.shouldDiscard:
             continue
 
-        if modifier.should_force_secondary:
+        if modifier.shouldForceSecondary:
             line['secondary'] = True
 
         line['start'] = str(timedelta(seconds=pytimeparse.parse(line['start'])) + modifier.offset)
         line['end'] = str(timedelta(seconds=pytimeparse.parse(line['end'])) + modifier.offset)
+
+        if modifier.shouldOverwriteStyle:
+            line['breakpoints'] = modifier.breakpoints
+            line['actors'] = modifier.actors
 
         outDetailedLyrics.append(line)
 
